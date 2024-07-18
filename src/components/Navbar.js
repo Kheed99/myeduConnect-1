4867134b737ae5fa0124b47e2cc5f5e7f9528ba0
SharpLad <143443228+yrezvani@users.noreply.github.com>
2024-07-18 19:07:53 +0100

    Update header background for better contrast and add Font Awesome icons

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,28 +6,60 @@ import './Navbar.css';
 import logo from '../images/logo.png';
 
 const Navbar = () => {
-  return (
-    <nav className="navbar">
-      <div className="navbar-logo">
-        <Link to="/">
-          <img src={logo} alt="eduConnect Logo" />
-        </Link>
-      </div>
-      <ul className="navbar-links">
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/about">About Us</Link></li>
-        <li><Link to="/courses">Courses</Link></li>
-        <li><Link to="/lms">LMS</Link></li>
-        <li><Link to="/lmis">LMIS</Link></li>
-        <li><Link to="/pricing">Pricing</Link></li>
-        <li><Link to="/contact">Contact</Link></li>
-      </ul>
-      <div className="navbar-buttons">
-        <Link to="/signup" className="signup-button">Sign Up</Link>
-        <Link to="/login" className="login-button">Login</Link>
-      </div>
-    </nav>
-  );
+    return (
+        <nav className="navbar">
+            <div className="navbar-logo">
+                <Link to="/">
+                    <img src={logo} alt="eduConnect Logo" />
+                </Link>
+            </div>
+            <ul className="navbar-links">
+                <li>
+                    <Link to="/">
+                        <i className="fas fa-home"></i> Home
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/about">
+                        <i className="fas fa-info-circle"></i> About Us
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/courses">
+                        <i className="fas fa-book"></i> Courses
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/lms">
+                        <i className="fas fa-chalkboard-teacher"></i> LMS
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/lmis">
+                        <i className="fas fa-tags"></i> LMIS
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/pricing">
+                        <i className="fas fa-tags"></i> Pricing
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/contact">
+                        <i className="fas fa-envelope"></i> Contact
+                    </Link>
+                </li>
+            </ul>
+            <div className="navbar-buttons">
+                <Link to="/signup" className="signup-button">
+                    <i className="fas fa-user-plus"></i> Sign Up
+                </Link>
+                <Link to="/login" className="login-button">
+                    <i className="fas fa-sign-in-alt"></i> Login
+                </Link>
+            </div>
+        </nav>
+    );
 };
 
 export default Navbar;
